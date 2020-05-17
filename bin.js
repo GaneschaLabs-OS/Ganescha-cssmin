14be352831fb0e9af85e2e9c07357cb21bf02b9a
oliverjessner <oliver.jessner@gmail.com>
2020-05-17 16:29:46 +0200

    gosh I am lazy

--- bin.js
+++ bin.js
@@ -3,11 +3,17 @@
 const cssmin = require('ganescha-cssmin');
 const fs = require('fs');
 const [, input, output] = process.argv;
-const css = fs.readFileSync(input, 'utf8');
 
-fs.writeFile(output, min, function (err) {
+fs.readFile(input, 'utf8', function (err, content) {
     if(err) {
         return console.log(err);
     }
-    console.log('ganescha-cssmin done');
-}); 
+
+    fs.writeFile(output, cssmin(content), function (err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log('ganescha-cssmin done');
+    }); 
+});
+
